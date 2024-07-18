865ab2d4b59258a58de4ca184e5429229b58e746
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-07-18 14:31:33 +0900

    [Feat]: DTO 추가

---
+++ Pokedex/DTO/CommonDTO/PokedexListNamedElementDTO.swift
@@ -0,0 +1,13 @@
+//
+//  PokedexListNamedElementDTO.swift
+//  Pokedex
+//
+//  Created by 최승범 on 7/18/24.
+//
+
+import Foundation
+
+struct PokedexListNamedElementDTO: Decodable {
+    let name: String
+    let url: String
+}
---
+++ Pokedex/DTO/PokemonDTO.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonDTO.swift
+//  Pokedex
+//
+//  Created by 최승범 on 7/18/24.
+//
+
+import Foundation
+
+struct PokemonDTO: Decodable {
+    let id: Int
+    let name: String
+    let heght: Int
+    let weight: Int
+    let sprites: [Sprite]
+    let types: [PokemonType]
+}
+
+struct PokemonType: Decodable {
+    let slot: Int
+    let type: [PokedexListNamedElementDTO]
+}
+
+struct Sprite: Decodable {
+    let front_default: String
+    let front_shiny: String
+    let front_female: String
+    let front_shiny_female: String
+}
+
