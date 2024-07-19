a24c61e336f134080ac53ef3e9d36520a2de7e76
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-07-19 14:08:21 +0900

    [Test]: 실제 네트워크 통신 테스트 추가, 기존의 비동기 테스트 확인 가능하게 변경

--- PokedexTests/PokedexTests.swift
+++ PokedexTests/PokedexTests.swift
@@ -10,49 +10,81 @@ import Combine
 import Alamofire
 
 final class PokedexTests: XCTestCase {
-
-    private var cancellables: Set<AnyCancellable> = []
     
-    override class func setUp() {
+    private var cancellables: Set<AnyCancellable> = []
+    private var mockPokedexData: MockPokedexData!
+    
+    override func setUp() {
         super.setUp()
-        
+        mockPokedexData = MockPokedexData()
+    }
+    
+    
+    override func tearDown() {
+        super.tearDown()
+        cancellables.removeAll()
+        mockPokedexData = nil
     }
     
     func test_네트워크_Mock() {
-        let mockPokedexData = MockPokedexData()
-        
         
         let config = URLSessionConfiguration.ephemeral
         config.protocolClasses = [MockURLProtocol.self]
         let session = Session(configuration: config)
         
         // Mock 데이터 주입
-        MockURLProtocol.requestHandler = { request in
+        MockURLProtocol.requestHandler = { [weak self] request in
             let response = HTTPURLResponse(url: request.url!,
                                            statusCode: 200,
                                            httpVersion: nil,
                                            headerFields: nil)!
             
-            let data = mockPokedexData.mockData()
+            let data = self?.mockPokedexData.mockData()
             
             return (response, data)
         }
         
         let networkManager = NetworkManager(session: session)
-        
+        let expectation = self.expectation(description: "Mock 네트워크 통신")
         
         // Mock데이터 비교
         networkManager.fetchData(PokedexDTO.self,
                                  router: .pokedex(start: 0, end: 2))
         .sink { completion in
-            
-        } receiveValue: { data in
+            print ("completion: \(completion)")
+        } receiveValue: { [weak self] data in
+            //결과
+            expectation.fulfill()
             XCTAssertEqual(data,
-                           mockPokedexData.pokedexData)
+                           self?.mockPokedexData.pokedexData)
         }
         .store(in: &cancellables)
+        
+        waitForExpectations(timeout: 5.0)
+    }
+    
+    func test_네트워크_실제통신() {
+        
+        //준비
+        let networkManager = NetworkManager()
+        let expectation = self.expectation(description: "Mock 네트워크 통신")
+        
+        // 실행
+        networkManager.fetchData(PokedexDTO.self,
+                                 router: .pokedex(start: 0, end: 2))
+        .sink { completion in
+            
+        } receiveValue: { [weak self] data in
+            // 결과
+            expectation.fulfill()
+            XCTAssertEqual(data,
+                           self?.mockPokedexData.pokedexData)
+        }
+        .store(in: &cancellables)
+        
+        waitForExpectations(timeout: 5.0)
+        
     }
     
     
-
 }
